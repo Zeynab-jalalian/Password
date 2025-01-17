fb67ab60c7c73d9780d3316bc972d6d21709d5ca
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-17 12:52:03 +0330

    access querySelector

---
+++ script.js
@@ -0,0 +1,6 @@
+let passwordInput=document.querySelector('.input-box input');
+let copyIcon=document.querySelector('.input-box span');
+let passIndicator=document.querySelector('.pass_indicator');
+let lengthSlider=document.querySelector('.pass_length input');
+let options=document.querySelectorAll('.option input');
+let generateBtn=document.querySelector('.generate_password');
