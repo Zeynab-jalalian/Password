c7d08116e86596840a4822a831859553b56faa17
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-17 14:20:55 +0330

    js step 2

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let passIndicator=document.querySelector('.pass_indicator');
 let lengthSlider=document.querySelector('.pass_length input');
 let options=document.querySelectorAll('.option input');
 let generateBtn=document.querySelector('.generate_password');
+let counterSlider=document.querySelector('.pass_length span');
+
+const characters={
+    lowercase:"abcdefghijklmnopqrstuvwxyz",
+    uppercase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers:"0123456789",
+    symbols:"~!@#$%^&*()-+[]{}<>|:;.,"
+}
+
+let generatePassword=function(){
+    let passLength=lengthSlider.value;
+    let staticPassword="";
+}
+const updateSlider=function(){
+    counterSlider.innerHTML=lengthSlider.value;
+}
+updateSlider();
+generateBtn.addEventListener('click',generatePassword);
+lengthSlider.addEventListener('input',updateSlider);
